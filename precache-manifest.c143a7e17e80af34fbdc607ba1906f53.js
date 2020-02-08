9e5f5d54048b68517fe75e9c2536a1599f0ce876
Jonathan Irwin <jonoirwinrsa@gmail.com>
2020-02-08 17:44:45 +0200

    Updates

---
+++ precache-manifest.c143a7e17e80af34fbdc607ba1906f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da161eacd58f51b9d63a5b1253cc463",
+    "url": "/cosuno-application/index.html"
+  },
+  {
+    "revision": "f1b08a7ade6c1576851e",
+    "url": "/cosuno-application/static/css/2.1e105a3b.chunk.css"
+  },
+  {
+    "revision": "f1b08a7ade6c1576851e",
+    "url": "/cosuno-application/static/js/2.801e1b28.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/cosuno-application/static/js/2.801e1b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd923cc48ea73584b04",
+    "url": "/cosuno-application/static/js/main.3652bfeb.chunk.js"
+  },
+  {
+    "revision": "905efaf641506655da61",
+    "url": "/cosuno-application/static/js/runtime-main.0c95549e.js"
+  }
+]);
