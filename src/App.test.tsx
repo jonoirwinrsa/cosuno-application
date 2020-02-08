82afe399d75cef4e3a109703ddaf91d020f3e58d
Jonathan Irwin <jonoirwinrsa@gmail.com>
2020-02-08 15:38:52 +0200

    test(homepage): adds basic navigation test and basic test utils with router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { fireEvent } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+import { renderWithRouter } from "./testUtils";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("full app rendering/navigating", () => {
+  const { container, getAllByText } = renderWithRouter(<App />);
+
+  // we should start on the homepage
+  // normally not the best testing practice to use the query selector but in this case we do care that the page has an h1
+  expect(container.querySelector("h1").textContent).toBe("Hello from South Africa 👋");
+
+  fireEvent.click(getAllByText(/companies/i)[0]);
+  expect(container.querySelector("h1").textContent).toBe("Companies");
 });
