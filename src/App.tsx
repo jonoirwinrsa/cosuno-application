799471420b52dac2185a7ac8f14af0aa7e735630
Jonathan Irwin <jonoirwinrsa@gmail.com>
2020-02-08 15:37:40 +0200

    feat(companies): adds basic companies page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Layout } from "antd";
 import React from "react";
 import { Route, Switch } from "react-router";
 import Navigation from "./components/Navigation";
+import CompaniesPage from "./pages/CompaniesPage";
 import HomePage from "./pages/HomePage";
 
 const App = () => (
@@ -10,6 +11,7 @@ const App = () => (
     <Layout.Content style={{ maxWidth: "1200px" }}>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/companies" component={CompaniesPage} />
       </Switch>
     </Layout.Content>
   </Layout>
---
+++ src/pages/CompaniesPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CompaniesPage = () => {
+  return (
+    <div style={{ padding: "20px 50px" }}>
+      <h1>Companies</h1>
+    </div>
+  );
+};
+
+export default CompaniesPage;
